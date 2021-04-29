a6be948ab54ab8155a995a717805583f77eb84b5
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-04-29 13:54:20 -0500

    commit tree 451a8ebb375e30c3f70d0c99695b302867b746cc

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(wins){ 
+    const result = wins.find(win => win.result === "W" );
+   if (result){
+       return result.year 
+   }
+   else return undefined;
+  
+   
+}
+ 
